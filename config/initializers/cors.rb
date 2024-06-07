9c3c2f89b2b53ceeac2626625be4c652f28d6470
Andre Costa <andrecondecosta@gmail.com>
2024-06-07 17:31:58 +0100

    Fix signs botton

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'https://cdphotografy.onrender.com'
+      origins 'http://localhost:4040', 'https://cdphotografy.onrender.com'
 
       resource "*",
         headers: :any,
